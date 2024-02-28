0325b841cd8e27afcfbf7f3147a674271145a15a
danilogalisteu <danilogalisteu@gmail.com>
2024-02-27 22:26:09 -0300

    added argv parsing to main

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 
-console.log('hello world')
+const { argv } = require('node:process');
+
+
+function main(){
+    if (argv.length < 3) {
+        console.log('too few arguments')
+        return
+    }
+    if (argv.length > 3) {
+        console.log('too many arguments')
+        return
+    }
+    const baseURL = argv[2]
+    console.log(`webcrawler is starting at ${baseURL}`)
+}
+
+
+main()
